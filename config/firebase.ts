1488dbbe80e073e091272fe6b1500a30ec548502
Aaro <aaro.heroja@gmail.com>
2022-10-31 16:32:03 +0200

    made firebase.ts.example file to be template of firebase.ts file

---
+++ config/firebase.ts
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+export const db = getFirestore()
