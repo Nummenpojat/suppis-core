53da906a9d4c48443006ae6e559462e4107c2182
Aaro <aaro.heroja@gmail.com>
2022-11-03 18:10:01 +0200

    now you can make new whatsapp session and listen for messages

--- bin/index.ts
+++ bin/index.ts
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 import {Command} from 'commander';
-import {newWhatsappSession} from "../src/modules/whatsapp/main";
+import {deleteWhatsapp, listenWhatsapp, newWhatsappSession} from "../src/modules/whatsapp/main";
 
 const program = new Command();
 
@@ -14,6 +14,18 @@ program.command('new')
   .arguments('<provider>')
   .action(() => {
     newWhatsappSession()
+  });
+
+program.command('listen')
+  .description('Listen for messages. Options at this point are: Whatsapp')
+  .arguments('<provider>')
+  .action((provider) => {
+    if (provider == "whatsapp") {
+      console.log(`Using provider ${provider}`)
+      listenWhatsapp()
+    } else {
+      console.error(`Provider ${provider} was invalid`)
+    }
   });
 
 program.parse();
--- src/modules/whatsapp/main.ts
+++ src/modules/whatsapp/main.ts
@@ -1,27 +1,40 @@
-import {Message, Client} from "whatsapp-web.js";
+import {Client, LocalAuth, Message} from "whatsapp-web.js";
 const qrcode = require('qrcode-terminal');
 
-const client = new Client({});
+
+const client = new Client({
+  authStrategy: new LocalAuth({ dataPath: "./config/whatsapp/session"})
+});
 
 export const newWhatsappSession = () => {
+
+  client.on('ready', () => {
+    console.log('Client is ready!');
+  });
 
   client.on('qr', (qr: string) => {
     console.clear();
     qrcode.generate(qr, {small: true});
   });
 
-  client.on('ready', () => {
-    console.log('Client is ready!');
-
-  });
-
-  client.on('message', (message: Message) => {
-    console.log(message.body);
-  });
-
   client.initialize();
 
 }
+
+export const listenWhatsapp = () => {
+
+  client.on('ready', () => {
+    console.log('Client is ready!');
+  });
+
+  client.on('message', (message: Message) => {
+    console.log(message.from)
+    console.log(message.body);
+  });
+
+}
+
+
 
 
 
