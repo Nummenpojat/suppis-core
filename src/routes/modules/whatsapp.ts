fa975d16c8137b948f62692dabde829ca6d9fc69
Aaro <aaro.heroja@gmail.com>
2022-11-10 15:31:13 +0200

    route to send message to list of people

--- src/routes/modules/whatsapp.ts
+++ src/routes/modules/whatsapp.ts
@@ -1,5 +1,6 @@
 import * as express from "express"
-import {sendMessage} from "../../modules/whatsapp/sendMessage";
+import {sendBulkMessage, sendMessage} from "../../modules/whatsapp/sendMessage";
+
 export const router = express.Router()
 
 router.use(express.json())
@@ -9,7 +10,19 @@ router.get('/', (req: any, res: any) => {
 })
 
 router.post('/send/one', (req, res) => {
-    res.status(202).send('Message received. Sending message!')
-    console.log(req.body.number, req.body.message)
-    sendMessage(req.body.number, req.body.message)
+
+  // Sending status message to approve request
+  res.status(202).send('Message received. Sending message!')
+
+  // Sending message to number that was given
+  sendMessage(req.body.number, req.body.message)
+})
+
+router.post('/send/list', (req, res) => {
+
+  // Sending status message to approve request
+  res.status(202).send('Message received. Sending messages!')
+
+  // Sending messages to list of people
+  sendBulkMessage(req.body.message);
 })
