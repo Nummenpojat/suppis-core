e1cd78606982a79028ce513d79e3f66cd753ec99
Aaro <aaro.heroja@gmail.com>
2022-11-12 20:18:35 +0200

    api endpoint to send a simple ad

--- src/api/modules/events.ts
+++ src/api/modules/events.ts
@@ -1,21 +1,19 @@
 import {Router} from "express";
 import {db} from "../../main";
 import {ScoutingEvent} from "../../types/event";
+import {sendAd} from "../../modules/events/commands/sendAd";
 
 const router = Router()
 
 router.get('/', async (req: any, res: any) => {
   const eventSnap = await db.collection("events").doc("pkXvtuCJ6FijdL92Ftc8").get()
   const event: ScoutingEvent = eventSnap.data() as ScoutingEvent
-  console.log()
-  res.send({
-    message: ` Hei! Oletko kuullut tapahtumasta nimeltä ${event.name}
-  ${event.description}
-  Tapahtuma ajankohta on: ${new Date(event.date.starts._seconds * 1000)} - ${new Date(event.date.ends._seconds * 1000)}
-  ${event.linkToEventCalendar}
-  `,
-    rawEventData: event
-  })
+  console.log(event)
+  res.send(event)
+})
+
+router.post('/sendAd', (req: any, res: any) => {
+  sendAd()
 })
 
 /**
--- src/modules/events/commands/sendAd.ts
+++ src/modules/events/commands/sendAd.ts
@@ -5,11 +5,14 @@ import {ScoutingEvent} from "../../../types/event";
 export const sendAd = async () => {
 
   // Getting event data from firebase to get details to send as ad
-  const eventSnap = await db.collection("persons").doc("pkXvtuCJ6FijdL92Ftc8").get()
-  const event = eventSnap.data()
+  const eventSnap = await db.collection("events").doc("pkXvtuCJ6FijdL92Ftc8").get()
+  const event = eventSnap.data() as ScoutingEvent
   console.log(event)
 
-  /*sendBulkMessage(`
-  Hei! Tiesitkö että on olemassa tapahtuma ${event.}
-  `)*/
+  sendBulkMessage(`
+  Hei! Oletko kuullut tapahtumasta nimeltä ${event.name}
+  ${event.description}
+  Tapahtuma ajankohta on: ${new Date(event.date.starts._seconds * 1000)} - ${new Date(event.date.ends._seconds * 1000)}
+  ${event.linkToEventCalendar}
+  `)
 }
