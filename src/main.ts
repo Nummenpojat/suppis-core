0e9d942ef59a77d30316db3cc114b4b25f8825a0
Aaro <aaro.heroja@gmail.com>
2022-11-28 18:09:55 +0200

    made function that verifies user on request with firebase auth

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {credential} from "firebase-admin";
 import * as express from "express"
 import {whatsappRouter} from "./api/modules/whatsapp";
 import {eventsRouter} from "./api/modules/events";
+import {getAuth} from "firebase-admin/auth";
 const cors = require("cors")
 
 /**
@@ -11,20 +12,40 @@ const cors = require("cors")
  * @todo Replace file path with your own firebase admin sdk secret key file
   */
 const ServiceAccount = require('../config/firebase-admin-secrets/suppis-firebase-admin-secrets.json');
-const app = initializeApp({
+const firebase = initializeApp({
   credential: credential.cert(ServiceAccount)
 });
 
-/**
- * Firestore database instance <br/>
- * It uses old namespaced environment so don't be confused
- */
-export const db = getFirestore(app);
+// Firestore database instance
+export const db = getFirestore(firebase);
 
 const api = express()
 const PORT = 3001
 
+const checkAuth = (req: any, res: any, next: any) => {
+  if (req.headers.idtoken) {
+    const idToken = req.headers.idtoken.toString()
+    getAuth().verifyIdToken(idToken)
+      .then((result) => {
+        if (result.email_verified && result.email == "aaro.heroja@nummenpojat.fi") {
+          console.log("ok!")
+          next()
+        } else {
+          console.log("wrong email")
+          res.status(403).send("Unauthorized!")
+        }
+      })
+      .catch(() => {
+        res.status(403).send("Unauthorized!")
+      })
+  } else {
+    res.status(403).send("Unauthorized!")
+  }
+}
+
 api.use(cors())
+api.use("/", checkAuth)
+
 api.use('/modules/whatsapp', whatsappRouter)
 api.use('/modules/events', eventsRouter)
 
