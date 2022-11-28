f2f143e559cbc5df7134f314e2d814f123c1e145
Aaro <aaro.heroja@gmail.com>
2022-11-28 19:27:17 +0200

    cleaned auth code

---
+++ src/auth.ts
@@ -0,0 +1,36 @@
+import {NextFunction, Request, Response} from "express";
+import {getAuth} from "firebase-admin/auth";
+
+/**
+ * Verifies authentication that came with API request
+ * @param req
+ * @param res
+ * @param next
+ */
+export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
+  if (verifyIdToken(req.headers.idtoken)) {
+    console.log("ID token passed all tests")
+    next()
+  }
+  res.status(403).send("Unauthorized!")
+}
+
+const verifyIdToken = (idToken: string | string[] | undefined): boolean => {
+
+  // Verifies that ID token is a string and not something else
+  if (typeof idToken === "string") {
+
+    // Verifies ID token to ensure correct access right to API
+    getAuth().verifyIdToken(idToken)
+      .then((result) => {
+        if (result.email_verified && result.email == "aaro.heroja@nummenpojat.fi") {
+          return true
+        }
+        return false
+      })
+      .catch(() => {
+        return false
+      })
+  }
+  return false
+}
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import {getFirestore} from "firebase-admin/firestore";
 import {initializeApp} from "firebase-admin/app";
 import {credential} from "firebase-admin";
 import * as express from "express"
+import {NextFunction, Request, Response} from "express"
 import {whatsappRouter} from "./api/modules/whatsapp";
 import {eventsRouter} from "./api/modules/events";
 import {getAuth} from "firebase-admin/auth";
+import {checkAuth} from "./auth";
 const cors = require("cors")
 
 /**
@@ -22,30 +24,8 @@ export const db = getFirestore(firebase);
 const api = express()
 const PORT = 3001
 
-const checkAuth = (req: any, res: any, next: any) => {
-  if (req.headers.idtoken) {
-    const idToken = req.headers.idtoken.toString()
-    getAuth().verifyIdToken(idToken)
-      .then((result) => {
-        if (result.email_verified && result.email == "aaro.heroja@nummenpojat.fi") {
-          console.log("ok!")
-          next()
-        } else {
-          console.log("wrong email")
-          res.status(403).send("Unauthorized!")
-        }
-      })
-      .catch(() => {
-        res.status(403).send("Unauthorized!")
-      })
-  } else {
-    res.status(403).send("Unauthorized!")
-  }
-}
-
 api.use(cors())
 api.use("/", checkAuth)
-
 api.use('/modules/whatsapp', whatsappRouter)
 api.use('/modules/events', eventsRouter)
 
