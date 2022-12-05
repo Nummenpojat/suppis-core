b47755941fd4ca0cb5ac21dfb6f2c5471cb2342e
Aaro <aaro.heroja@gmail.com>
2022-12-05 22:11:13 +0200

    added websockets and on ws connect app is also connected to whatsapp

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,16 @@ import {whatsappRouter} from "./api/modules/whatsapp";
 import {eventsRouter} from "./api/modules/events";
 import {checkAuth, setUserToAdmin} from "./auth";
 import {json} from "express";
+
 const cors = require("cors")
+import {WebSocketServer} from "ws";
+import {client} from "./modules/whatsapp/main";
+import {Message} from "whatsapp-web.js";
 
 /**
  * Constant that holds Firebase admin sdk service account <br/>
  * @todo Replace file path with your own firebase admin sdk secret key file
-  */
+ */
 const ServiceAccount = require('../config/firebase-admin-secrets/suppis-firebase-admin-secrets.json');
 const firebase = initializeApp({
   credential: credential.cert(ServiceAccount)
@@ -20,24 +24,53 @@ const firebase = initializeApp({
 // Firestore database instance
 export const db = getFirestore(firebase);
 
-const api = express()
 const PORT = 3001
 
-api.use(cors())
-api.use(json())
-api.use("/", checkAuth)
-api.use('/modules/whatsapp', whatsappRouter)
-api.use('/modules/events', eventsRouter)
+const http = express()
+const socketServer = new WebSocketServer({
+  port: 3002
+})
 
-api.get('/', (req: any, res: any) => {
+socketServer.on('connection', (socket) => {
+  client.initialize()
+
+  client.on("qr", (qr) => {
+    socket.send(qr)
+  })
+
+  client.on("ready", () => {
+
+    socket.send("Client is ready")
+
+    socket.on('message', (rawMessage: any) => {
+      const message: { type: string, number: string, message: string } = JSON.parse(rawMessage)
+      console.log(message)
+      if (message.type == "one") {
+
+      }
+      if (message.type == "list") {
+
+      }
+    })
+  })
+
+})
+
+http.use(cors())
+http.use(json())
+http.use("/", checkAuth)
+http.use('/modules/whatsapp', whatsappRouter)
+http.use('/modules/events', eventsRouter)
+
+http.get('/', (req: any, res: any) => {
   res.send("This is Suppis!")
 })
 
-api.put('/admin', (req, res) => {
+http.put('/admin', (req, res) => {
   setUserToAdmin(req)
     .then(() => res.status(201).send(`${req.body.email} is now admin`))
     .catch((reason) => res.status(reason.status).send(reason.reason))
 })
 
-api.listen(PORT);
+http.listen(PORT);
 console.log(`App listening on port: ${PORT}`)
