fe36d49cc8917cca700b869e319834e9de9980a4
Aaro <aaro.heroja@gmail.com>
2022-12-17 14:48:52 +0200

    sending messages to lists now possible

--- src/modules/whatsapp/commands/sendMessageToList.ts
+++ src/modules/whatsapp/commands/sendMessageToList.ts
@@ -1,13 +1,11 @@
-import {client} from "../main";
+import {client, isClientReady} from "../main";
 
 /**
  * Send same message to list of people
  * @param message Text that is sent to list provided
  * @param persons Array that contains numbers of people to send the message
  * */
-export const sendMessageToList = async (message: string, persons: string[]) => {
-
-  console.log('Sending messages...');
+export const sendMessageToList = async (message: string, numbers: string[]) => {
 
   // Checking that message is not empty
   if (message == "" || message == null) {
@@ -15,23 +13,30 @@ export const sendMessageToList = async (message: string, persons: string[]) => {
   }
 
   // Sending message to each on the list
-  persons.forEach((person: any) => {
+  for (const num of numbers) {
 
     //Making chat id from phone number to use at client.sendMessage to identify where to send the message
-    let chatId = person.number + "@c.us"
+    let chatId = num + "@c.us"
 
     // Removing + at the start if it exits so the phone number is in right format
     if (chatId.startsWith('+')) {
       chatId = chatId.substring(1)
     }
 
-    // Sending message to chosen chat
-    client.sendMessage(chatId, message)
-      .then((message) => {
-        console.log(`Message ${message.body} sent to ${person.name}`);
-      })
-      .catch((error: Error) => {
-        throw error
-      })
-  })
+    try {
+
+      isClientReady()
+
+      // Sending message to chosen chat
+      const returnMessage = await client.sendMessage(chatId, message)
+
+      console.log(`Message ${returnMessage.body} sent to ${num}`);
+
+    } catch (error: any) {
+      if (error.message != undefined) {
+        throw error.message
+      }
+      throw error
+    }
+  }
 }
